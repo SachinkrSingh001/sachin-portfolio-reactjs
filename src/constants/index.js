65682f376379d23371d7ecc69423e1eeccc6606a
SachinkrSingh001 <90031498+SachinkrSingh001@users.noreply.github.com>
2025-01-12 21:34:17 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,79 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+
+
+export const HERO_CONTENT = `A Full stack developer passionate about coding and problem-solving. With knowledge in programming languages like HTML, Tailwind CSS, Javascript, ReactJS, ExpressJS, MongoDB, NodeJS. I'm eager to tackle new challenges and contribute to impactful projects. I thrive on continuous learning and collaboration, and I'm excited to make my mark in the world of Full Stack development.`;
+
+export const ABOUT_TEXT = `I am Sachin Kumar Singh, a recent Computer Science Engineering graduate with a passion for Full Stack Development and a strong foundation in web technologies. I am skilled in front-end development with HTML, Tailwind CSS, JavaScript, and React.js, as well as back-end frameworks like Node.js with experience in databases such as MongoDB and MySQL. `;
+export const ABOUT_TEXT1 = `These skills enable me to build responsive, scalable, and user-friendly web applications. A quick learner with a problem-solving mindset, I am constantly exploring new tools and frameworks to enhance my expertise. Actively seeking entry-level opportunities, I am eager to contribute to innovative projects and collaborate with dynamic teams to create impactful solutions. Let’s connect and explore how I can add value to your organization.` 
+
+export const EXPERIENCES = [
+  {
+    year: "September 2024 - Present",
+    role: "Research and Development Intern",
+    company: "Kritikal Solutions Pvt. Ltd. ",
+    description: `Conducted business development research to identify potential collaborators in AI, ML, and embedded software technologies. Created and managed a contact database of industry stakeholders using LinkedIn and CRM tools, facilitating strategic outreach and engagement.`,
+    technologies: ["Microsoft Excel", "Google Sheets", "Sales Navigator", "Zoho", "Apollo"],
+  },
+  {
+    year: "September 2024 - December 2024",
+    role: "Backend Developer (3 Month)",
+    company: "Yugasa Software",
+    description: `Developed and enhanced chatbot functionalities by building APIs and managing databases using Node.js and MongoDB. Created a secure login page focusing on user authentication and backend integrations.`,
+    technologies: ["ReactJS", "NodeJS", "MongoDB", "Yugasa Bot", "Visual Studio"],
+  },
+  {
+    year: "August 2024 - Present",
+    role: "Full Stack Developer with NodeJS",
+    company: "Ducat Training Centre",
+    description: `Currently enrolled in an intensive Full Stack Developer course at Ducat, South Extension, covering modern technologies including HTML, Tailwind CSS, JavaScript, React.js, MongoDB, and Node.js. Actively building dynamic, responsive web applications and APIs as part of course projects, focusing on real-world problem-solving and best practices in full-stack development.`,
+    technologies: ["ReactJS", "NodeJS", "MongoDB", "Yugasa Bot", "Visual Studio"],
+  },
+  {
+    year: "July 2023 - August 2023",
+    role: "Javascript Developer",
+    company: "Digital Bhem Organisation",
+    description: `Designed and developed a number-guessing game using HTML, CSS, and JavaScript, focusing on interactive gameplay mechanics and responsive design. Hosted the project on GitHub for version control and deployment.`,
+    technologies: ["Html", "Css", "JavaScript"],
+  }];
+
+export const PROJECTS = [
+  {
+    title: "Disease Prediction Using ML",
+    image: project1,
+    description:
+      "A Machine Learning Model to Predict Disease by analyzing Patient data like age, Medical History etc. Worked with Different algorithms like Support Vector Machines and Random Forest.",
+    technologies: ["Google Colab ","Anaconda " , "Spyder " , "Streamlit "],
+  },
+  {
+    title: "Register-Login Page using Backend Technologies",
+    image: project2,
+    description:
+      "Designed a server side page for register and login of user to authenticate user and maintain its integrity of his data. ",
+    technologies: ["ReactJS", "ExpressJS", "MongoDB", "NodeJS", "JWT"],
+  },
+  {
+    title: "Portfolio using Frontend Technologies",
+    image: project3,
+    description:
+      "Designing a modern and interactive portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind CSS", "ReactJS", "Github", "VScode", "Vercel"],
+  },
+  {
+    title: "Virtual Assistant(Jerry)",
+    image: project4,
+    description:
+      "Created a Python-based virtual assistant named jerry. It uses If- Elif Statements and python kits to talk with the user.",
+    technologies: ["Python ", "speech_recognition ", "pyttsx3 ", "pywhatkit ", "datetime ", "Wikipedia ", "VS code "],
+  },
+];
+
+export const CONTACT = {
+  name: "Sachin Kumar Singh",
+  address: "Janakpuri, North-west Delhi, 110059 ",
+  phoneNo: "+918447870158 ",
+  email: "sachinkrsingh20@gmail.com",
+};
